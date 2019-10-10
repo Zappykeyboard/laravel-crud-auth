4b8a149bb5136b7b111f205b2a476e059f2f531e
Edoardo <a@Air-di-edo.lan>
2019-10-10 16:04:45 +0200

    aggiunto seeder

--- database/seeds/CatsSeeder.php
+++ database/seeds/CatsSeeder.php
@@ -1,16 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
+use App\Cat;
+class CatsSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(CatsSeeder::class);
+        factory(Cat::class, 30)->create();
     }
 }
